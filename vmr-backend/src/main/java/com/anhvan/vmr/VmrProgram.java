eb784bedbe1640409124cb6ee802f993866b5ea2
Dang Anh Van <danganhvan1999@gmail.com>
2020-08-24 11:20:32 +0700

    create backend backbone

---
+++ vmr-backend/src/main/java/com/anhvan/vmr/VmrProgram.java
@@ -0,0 +1,12 @@
+package com.anhvan.vmr;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VmrProgram {
+  private static final Logger LOGGER = LogManager.getLogger(VmrProgram.class);
+
+  public static void main(String[] args) {
+    LOGGER.debug("Hello world");
+  }
+}
