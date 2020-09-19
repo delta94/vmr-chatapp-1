b0e1d28143ef77e91535082d5fe51ad352473faa
Dang Anh Van <danganhvan1999@gmail.com>
2020-09-19 17:55:44 +0700

    Add test for ChatDbServiceImpl

---
+++ vmr-newbackend/src/test/java/com/anhvan/vmr/database/ChatDBServiceImplTest.java
@@ -0,0 +1,78 @@
+package com.anhvan.vmr.database;
+
+import com.anhvan.vmr.model.Message;
+import io.vertx.core.Handler;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PreparedQuery;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+import java.time.Instant;
+
+@ExtendWith(VertxExtension.class)
+@Log4j2
+@SuppressWarnings("unchecked")
+public class ChatDBServiceImplTest {
+  private static MySQLPool pool;
+
+  private static ChatDBServiceImpl chatDBService;
+
+  @BeforeAll
+  static void setUp() {
+    pool = Mockito.mock(MySQLPool.class);
+    DatabaseService databaseService = Mockito.mock(DatabaseService.class);
+    Mockito.when(databaseService.getPool()).thenReturn(pool);
+    chatDBService = new ChatDBServiceImpl(databaseService);
+  }
+
+  @Test
+  void testAddChat(VertxTestContext vertxTestContext) {
+    PreparedQuery<RowSet<Row>> preparedQuery =
+        (PreparedQuery<RowSet<Row>>) Mockito.mock(PreparedQuery.class);
+
+    Mockito.when(pool.preparedQuery(ChatDBServiceImpl.INSERT_MESSAGE)).thenReturn(preparedQuery);
+
+    Mockito.doAnswer(
+            invocationOnMock -> {
+              Tuple tuple = invocationOnMock.getArgument(0);
+              Assertions.assertEquals(1, tuple.getInteger(0));
+              Assertions.assertEquals(2, tuple.getInteger(1));
+              Assertions.assertEquals("Hello", tuple.getString(2));
+              vertxTestContext.completeNow();
+              return null;
+            })
+        .when(preparedQuery)
+        .execute(ArgumentMatchers.any(Tuple.class), ArgumentMatchers.any(Handler.class));
+
+    chatDBService.addChat(
+        Message.builder()
+            .senderId(1)
+            .receiverId(2)
+            .message("Hello")
+            .timestamp(Instant.now().getEpochSecond())
+            .build());
+  }
+
+  @Test
+  void testGetChat() {
+    PreparedQuery<RowSet<Row>> preparedQuery =
+        (PreparedQuery<RowSet<Row>>) Mockito.mock(PreparedQuery.class);
+
+    Mockito.when(pool.preparedQuery(ChatDBServiceImpl.GET_MESSAGES_QUERY))
+        .thenReturn(preparedQuery);
+
+    chatDBService.getChatMessages(1, 2, 0);
+
+    Mockito.verify(preparedQuery).execute(ArgumentMatchers.any(), ArgumentMatchers.any());
+  }
+}
