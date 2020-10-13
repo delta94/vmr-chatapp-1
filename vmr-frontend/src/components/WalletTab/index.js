2910c331d5bba5e659295c99b7100360cc981304
Dang Anh Van <danganhvan1999@gmail.com>
2020-10-13 23:33:11 +0700

    Fix some style

--- vmr-frontend/src/components/WalletTab/index.js
+++ vmr-frontend/src/components/WalletTab/index.js
@@ -7,13 +7,13 @@ export default function WalletTab() {
   return (
     <div className="conversation-list-scroll">
       <WalletTabItem
-        icon={<WalletOutlined style={{color: '#f5222d'}}/>}
+        icon={<WalletOutlined style={{color: '#389e0d'}}/>}
         title="Số Dư"
         description="Xem số dư, thông tin tài khoản"
         tabName="balance"
         path="/w/balance"/>
       <WalletTabItem
-        icon={<HistoryOutlined style={{color: '#597ef7'}}/>}
+        icon={<HistoryOutlined style={{color: '#096dd9'}}/>}
         title="Lịch sử"
         description="Xem lịch sử giao dịch"
         tabName="history"
