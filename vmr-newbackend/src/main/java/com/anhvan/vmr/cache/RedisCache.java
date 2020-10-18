da925bcad51b02b2ae5a7c7adc82361b207ba501
Dang Anh Van <danganhvan1999@gmail.com>
2020-10-18 21:28:09 +0700

    Clear cache when start the app

--- vmr-newbackend/src/main/java/com/anhvan/vmr/cache/RedisCache.java
+++ vmr-newbackend/src/main/java/com/anhvan/vmr/cache/RedisCache.java
@@ -1,9 +1,12 @@
 package com.anhvan.vmr.cache;
 
 import com.anhvan.vmr.configs.CacheConfig;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.redisson.Redisson;
+import org.redisson.api.RKeys;
 import org.redisson.api.RedissonClient;
 
 import javax.inject.Inject;
@@ -17,9 +20,34 @@ public class RedisCache {
   public RedisCache(CacheConfig cacheConfig) {
     try {
       redissonClient = Redisson.create(cacheConfig.getRedisConfig());
-      log.info("Connected to redis");
+      flushAll()
+          .onComplete(
+              ar -> {
+                if (ar.succeeded()) {
+                  log.info("Connected to redis");
+                } else {
+                  log.info("Fail to clear cache");
+                }
+              });
     } catch (Exception e) {
       log.fatal("Error when create redisson instance", e);
     }
+  }
+
+  public Future<Void> flushAll() {
+    Promise<Void> promise = Promise.promise();
+
+    RKeys keys = redissonClient.getKeys();
+    keys.flushallAsync()
+        .onComplete(
+            (unused, throwable) -> {
+              if (throwable != null) {
+                promise.fail(throwable);
+              } else {
+                promise.complete();
+              }
+            });
+
+    return promise.future();
   }
 }
