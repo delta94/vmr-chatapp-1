8684227b59904b14004d870fc384d5e5c9e73960
Dang Anh Van <danganhvan1999@gmail.com>
2020-10-20 17:10:30 +0700

    Add build file for react

--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ cd ..
 cp vmr-newbackend/target/vmr-newbackend-1.0-SNAPSHOT.jar vmr-docker/vmr-backend/
 mv vmr-docker/vmr-backend/vmr-newbackend-1.0-SNAPSHOT.jar vmr-docker/vmr-backend/bundle.jar
 cp vmr-newbackend/src/main/resources/* vmr-docker/vmr-backend/configs
+
+# build frontend
+cd vmr-frontend
+yarn build
+cd ..
+mkdir -p vmr-docker/vmr-frontend/build
+cp -R vmr-frontend/build/ vmr-docker/vmr-frontend/build/
+cp vmr-frontend/server.js vmr-docker/vmr-frontend
---
+++ vmr-docker/vmr-frontend/build/build/precache-manifest.4804e1ebe6513d5e589b19f7c8d0c0a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8870c2cc78073e0c088f778d648691f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07bb8dda40cbeba8e22a",
+    "url": "/static/css/3.220ccb42.chunk.css"
+  },
+  {
+    "revision": "1a0f20d8632613331de0",
+    "url": "/static/css/main.21fd243f.chunk.css"
+  },
+  {
+    "revision": "31af95cc3fa18f004d61",
+    "url": "/static/js/0.dcf025ff.chunk.js"
+  },
+  {
+    "revision": "07bb8dda40cbeba8e22a",
+    "url": "/static/js/3.5c2a111b.chunk.js"
+  },
+  {
+    "revision": "ce84f45c6e623bd7d8c4f820fd2feb3f",
+    "url": "/static/js/3.5c2a111b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc77ce179d4bc725640b",
+    "url": "/static/js/4.ca5f2f6a.chunk.js"
+  },
+  {
+    "revision": "8b39f69f794f7c9e5e9d",
+    "url": "/static/js/5.2c4474ad.chunk.js"
+  },
+  {
+    "revision": "1a0f20d8632613331de0",
+    "url": "/static/js/main.b55a424f.chunk.js"
+  },
+  {
+    "revision": "115787e45b4dfe2ec53d",
+    "url": "/static/js/runtime-main.fbdea150.js"
+  },
+  {
+    "revision": "68fa86345ee1d09d0a83053ff6e786a0",
+    "url": "/static/media/registerbg.68fa8634.jpg"
+  }
+]);
